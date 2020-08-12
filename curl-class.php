9fc67cf5ccd2c73d5c6a619188d8691d50ed35c7
James Blackwell <james@liquorice.marketing>
2020-08-12 14:21:37 +0100

    Initital commit of V2 - merged all functions into one

--- curl-class.php
+++ curl-class.php
@@ -1,85 +1,33 @@
-<?php 
-if(!class_exists("Curl")) {
+<?php
+declare(strict_types = 1);
+
+if (!class_exists('Curl')) {
     class Curl{
 
         private static $conn;
         private static $defaults = [
-            CURLOPT_HEADER => 0,                                        //Include header with request
-            CURLOPT_RETURNTRANSFER => 1,                                //
-            CURLOPT_TIMEOUT => 4,                                       //
+            CURLOPT_HEADER => 0,                    // Include header with request
+            CURLOPT_RETURNTRANSFER => 1,            // Return result on curl_exec()
+            CURLOPT_TIMEOUT => 4,                   // Set timeout to 4s
         ];
-
+        
         /**
-         * Initialises and executes a GET request using CURL
-         * 
-         * @param string $url The request URL
-         * @param array $options An associative array of CURLOPTs
-         * 
-         * @return $return The return from the request
+         * Curl::request()
+         * Makes a request
+         *
+         * @param string $method
+         * @param string $url
+         * @param array $options
+         * @param array $body
+         * @return string
          */
-        public static function get($url, $options = []) {
+        public static function request(string $method, string $url, array $options = [], array $body = []):string {
             self::$conn = curl_init();
 
             curl_setopt(self::$conn, CURLOPT_URL, $url);
-            curl_setopt_array(self::$conn, (array) self::$defaults + (array) $options);
-            try {
-                $result = curl_exec(self::$conn);
-                if (!$result) {
-                    throw new Exception();
-                }
-            } catch(Exception $e) {
-                echo curl_errno(self::$conn).' - '.curl_error(self::$conn);
-            }
-            curl_close(self::$conn);
-
-            return $result;
-        }
-
-        /**
-         * Initialises and executes a POST request using CURL
-         * 
-         * @param string $url The request URL
-         * @param array $options An associative array of CURLOPTs
-         * @param array $body The POST body
-         * 
-         * @return $return The return from the request
-         */
-        public static function post($url, $options=[], $body = null) {
-            self::$conn = curl_init();
-
-            curl_setopt(self::$conn, CURLOPT_URL, $url);
+            curl_setopt(self::$conn, CURLOPT_CUSTOMREQUEST, $method);
             curl_setopt(self::$conn, CURLOPT_POSTFIELDS, $body);
             curl_setopt_array(self::$conn, (array) self::$defaults + (array) $options);
-            try {
-                $result = curl_exec(self::$conn);
-                if (!$result) {
-                    throw new Exception();
-                }
-            } catch(Exception $e) {
-                echo curl_errno(self::$conn).' - '.curl_error(self::$conn);
-            }
-            curl_close(self::$conn);
-            return $result;
-
-        }
-
-        /**
-         * Initialises and executes a PATCH request using CURL
-         * 
-         * @param string $url The request URL
-         * @param array $options An associative array of CURLOPTs
-         * @param array $body The PATCH body
-         * 
-         * @return $return The return from the request
-         */
-        public static function patch($url, $options=[], $body = null) {
-            self::$conn = curl_init();
-
-            curl_setopt(self::$conn, CURLOPT_URL, $url);
-            curl_setopt(self::$conn, CURLOPT_POSTFIELDS, $body);
-            curl_setopt(self::$conn, CURLOPT_CUSTOMREQUEST, 'PATCH');
-            curl_setopt_array(self::$conn, (array) self::$defaults + (array) $options);
-
             try {
                 $result = curl_exec(self::$conn);
                 if (!$result) {
@@ -92,6 +40,5 @@ if(!class_exists("Curl")) {
             return $result;
         }
     }
-}
-
+};
 ?>
